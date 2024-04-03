478c4b43d67bc07b5be7c48b09fde025508f9c30
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-03 16:28:04 -0300

    [ :sparkles: ] | create: header - cpu > 6502 (app::header)

--- src/headers/6502.h
+++ src/headers/6502.h
@@ -1,5 +1,16 @@
+#include <cstdint>
+class Bus;
+
 class C6502 {
 public:
   C6502();
   ~C6502();
+
+  void CconnectBus(Bus *n) { bus = n; };
+
+private:
+  Bus *bus = nullptr;
+
+  uint8_t read(uint16_t addr);
+  void write(uint16_t addr, uint8_t data);
 };
