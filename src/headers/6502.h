f6307eeb3919c7cd7ef154c5240f488ba6a64530
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-03 16:42:19 -0300

    [ :sparkles: ] | setup: cpu - addressing modes (app::header)

--- src/headers/6502.h
+++ src/headers/6502.h
@@ -8,6 +8,20 @@ public:
 
   void CconnectBus(Bus *n) { bus = n; };
 
+  // Addressing Modes
+  uint8_t IMP();
+  uint8_t IMM();
+  uint8_t ZP0();
+  uint8_t ZPX();
+  uint8_t ZPY();
+  uint8_t REL();
+  uint8_t ABS();
+  uint8_t ABX();
+  uint8_t ABY();
+  uint8_t IND();
+  uint8_t IZX();
+  uint8_t IZY();
+
 public:
   enum FLAGS6502 {
     C = (1 << 0), // Carry Bit
